e75b1652b2ee02c88a956a6621f91be297df66f3
Bianca Cunha <biancunha@gmail.com>
2020-09-18 18:25:27 -0300

    Add files via upload

---
+++ Modulo01/03.js
@@ -0,0 +1,7 @@
+function temHabilidade(skills) {
+    return skills.indexOf("Javascript") !== -1;
+}
+
+var skills = ["Javascript", "ReactJS", "React Native"];
+
+console.log(temHabilidade(skills));
